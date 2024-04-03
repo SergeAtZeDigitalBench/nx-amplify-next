36bd6728748a50203cf85bd077425720d2cd5a1d
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-03 08:08:48 +0100

    Initial commit

---
+++ apps/fe-next-e2e/cypress.config.ts
@@ -0,0 +1,14 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: { default: 'nx run fe-next:start' },
+      ciWebServerCommand: 'nx run fe-next:serve-static',
+    }),
+    baseUrl: 'http://localhost:3000',
+  },
+});
