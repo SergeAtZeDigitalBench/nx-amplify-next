bc2b28a84a9171f58841591dac7725675b3b2dad
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-03 10:00:38 +0100

    feat: tailwind install setup, header nav

---
+++ apps/fe-next/src/components/Navigation/components/AuthButtons.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+const AuthButtons = (): JSX.Element => {
+  return (
+    <>
+      <li>
+        <button>signin</button>
+      </li>
+      <li>
+        <button>signup</button>
+      </li>
+    </>
+  );
+};
+
+export default AuthButtons;
