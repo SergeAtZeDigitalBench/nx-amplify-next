2084cb50b6ce6a8c96b9e25760dd57d55f8d7610
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-12 16:21:17 +0100

    fix: apps, fe, jest config, removed redundant setting

--- apps/fe-next/jest.config.ts
+++ apps/fe-next/jest.config.ts
@@ -2,9 +2,6 @@
 export default {
   displayName: 'fe-next',
   preset: '../../jest.preset.js',
-  globals: {
-    Uint8Array: Uint8Array,
-  },
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
@@ -12,7 +9,4 @@ export default {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/fe-next',
   setupFiles: ['./jest.polyfills.js'],
-  testEnvironmentOptions: {
-    customExportConditions: [''],
-  },
 };
