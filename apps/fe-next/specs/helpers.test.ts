87384a4ffcd16f46c01a907c0190608578823ed9
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-12 15:33:58 +0100

    fix: setup jest and msw, fixed global types in jest env

---
+++ apps/fe-next/specs/helpers.test.ts
@@ -0,0 +1,52 @@
+import { http, HttpResponse, delay } from 'msw';
+
+import type { HttpResponseResolver } from 'msw';
+
+import { serverWorker } from '../src/lib/msw/server';
+import { fetchPets, fetchPetsSsr } from '../src/components/PetsList/helpers';
+
+describe('PetsList/helpers', () => {
+  const { petsResolverError } = getTestData();
+
+  beforeAll(() => {
+    serverWorker.listen();
+  });
+  afterEach(() => serverWorker.resetHandlers());
+  afterAll(() => serverWorker.close());
+
+  describe('fetchPets', () => {
+    it('should return 3 pets on success', async () => {
+      const response = await fetchPets();
+
+      expect(response).toHaveLength(3);
+    });
+
+    it('should throw an error', async () => {
+      serverWorker.use(
+        http.get('https://api.example.com/pets', petsResolverError, {
+          once: true,
+        })
+      );
+      let errorMessage: null | string = null;
+
+      try {
+        await fetchPets();
+      } catch (error) {
+        errorMessage = error instanceof Error ? error.message : 'default error';
+      }
+
+      expect(errorMessage).toEqual('mock error message');
+    });
+  });
+});
+
+function getTestData() {
+  const petsResolverError: HttpResponseResolver = async () => {
+    await delay(500);
+    return HttpResponse.json({ error: 'mock error message' }, { status: 400 });
+  };
+
+  return {
+    petsResolverError,
+  };
+}
